a83c13ae5adb500d809ef596b08fc697bf8d4674
Andyshy <2217556994@qq.com>
2019-08-17 08:12:03 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "summer",
     version = "0.0.1",
     author = "Andy Yang",
     author_email = "2217556994@qq.com",
-    py_modules = ["summer"],
+    packages = ["summer"],
     license='MIT',
     url = "https://github.com/Andyshy/summer",
     description = "excel helper"
